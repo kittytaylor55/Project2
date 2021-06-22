395e27d94f70f3d380dc9c9a13b428c9ac33236e
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-06-21 20:33:55 -0700

    added bulma styling to main and home page

--- controllers/mainRoutes.js
+++ controllers/mainRoutes.js
@@ -2,10 +2,15 @@ const router = require('express').Router();
 
 // import models here
 
-//route for loading main page
+//route for loading main page with logon/sign up
 router.get('/', async (req, res) => {
   try {
-    res.render('home');
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.loggedIn) {
+      res.redirect('profile');
+      return;
+    }
+    res.render('home-login');
   } catch (err) {
     res.status(500).json(err);
   }
@@ -21,17 +26,17 @@ router.get('/profile', async (req, res) => {
 });
 
 //Get login page
-router.get('/login', async (req, res) => {
-  try {
-    // If the user is already logged in, redirect the request to another route
-    if (req.session.loggedIn) {
-      res.redirect('profile');
-      return;
-    }
-    res.render('login');
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
+// router.get('/login', async (req, res) => {
+//   try {
+//     // If the user is already logged in, redirect the request to another route
+//     if (req.session.loggedIn) {
+//       res.redirect('profile');
+//       return;
+//     }
+//     res.render('login');
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json(err);
+//   }
+// });
 module.exports = router;
